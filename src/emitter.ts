60ebaee71c80b473efff49fbe5fbfeae7b3dfa14
Kevin Saldana <ksaldana1@gmail.com>
2024-08-14 11:12:29 -0400

    play with imports

--- src/emitter.ts
+++ src/emitter.ts
@@ -4,6 +4,7 @@ import { parseHTTPService } from "./parse/index.js";
 
 export async function $onEmit(context: EmitContext) {
   const services = getAllHttpServices(context.program);
+  console.log(services);
   const service = services.at(0)!.at(0) as HttpService;
 
   parseHTTPService(service);

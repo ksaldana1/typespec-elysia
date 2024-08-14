d7ff305432b4ff9a16b694a22bc568595583d20c
Kevin Saldana <ksaldana1@gmail.com>
2024-08-14 12:17:21 -0400

    play with ideas around service composition

---
+++ gateway.ts
@@ -0,0 +1,29 @@
+import { petServer, todoServer } from "./defs";
+import { Elysia } from "elysia";
+import { UnionToIntersection } from "type-fest";
+
+export const app = new Elysia()
+  .use(petServer)
+  .use(todoServer) satisfies Gateway<[typeof petServer, typeof todoServer]>;
+
+type GatewaySchema<T extends Array<Elysia<any, any, any, any>>> = {
+  [K in keyof T]: {
+    definitions: T[K]["_types"]["Definitions"];
+    routes: T[K]["_routes"];
+  };
+};
+type Definitions<T extends Array<Elysia<any, any, any, any>>> =
+  UnionToIntersection<GatewaySchema<T>[number]["definitions"]>;
+
+type Routes<T extends Array<Elysia<any, any, any, any>>> = UnionToIntersection<
+  GatewaySchema<T>[number]["routes"]
+>;
+
+type Gateway<T extends Array<Elysia<any, any, any, any>>> = Elysia<
+  "",
+  false,
+  { decorator: {}; derive: {}; resolve: {}; store: {} },
+  Definitions<T>,
+  { macro: {}; macroFn: {}; schema: {} },
+  Routes<T>
+>;
